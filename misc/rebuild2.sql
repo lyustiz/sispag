851ba4e1d59ca09d8f742b6bf702128b4880ac3e
lyustiz <lyustiz@gmail.com>
2019-02-22 16:55:51 -0400

    update22022019

---
+++ misc/rebuild2.sql
@@ -0,0 +1,26 @@
+﻿select  
+	ingreso - instruccion as disponible,	
+	instruccion - pagado  instruccion,
+	(ingreso - instruccion)+ (instruccion - pagado) total
+
+from 
+
+(
+	SELECT (select sum(mo_ingreso) from pagos.ingreso where id_status = 1 )   ingreso, 
+	       SUM(MO_INSTRUCCION) instruccion, 
+	       SUM(MO_PAGO) pagado, moneda  FROM 
+	(
+		SELECT mo_instruccion,
+		       ( SELECT nb_moneda
+			   FROM pagos.moneda 
+			  WHERE id_moneda = ins.id_moneda) moneda,
+		    (SELECT sum(mo_final_pago) 
+		       FROM pagos.pago pag 
+		      WHERE pag.id_status = 31  
+			AND pag.id_instruccion = ins.id_instruccion) MO_PAGO
+		FROM pagos.instruccion ins
+		
+	) pago
+
+	group by moneda
+) total
